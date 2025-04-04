b28d056d606471d4f8e78d535c263a0f39e815a8
JanelieWang <1240117096@qq.com>
2025-04-04 10:36:00 +0800

    update

--- src/NSGA-II.py
+++ src/NSGA-II.py
@@ -1,7 +1,7 @@
 from scipy.io import loadmat
 import random
-
-
+import pickle
+import json
 
 # mr1 = flipLeftRight
 # mr2 = gaussian
@@ -44,31 +44,39 @@ def nsga(res_path,dir_path,dataset,model,budget,mr):
             all_pairs.append((labelSourceArray_mr[index], labelFollowUpArray_mr[index]))
     uniq_p = remove_duplicates(all_pairs)
 
-    return error_num ,uniq_p
+    return error_num ,uniq_p,selected_elements
 
 mrs = {'mr1':'flipLeftRight', 'mr2':'gaussian', 'mr3':'rotatePlus5deg', 'mr4':'colored', 'mr5':'brightness'}
 res_path = '/Users/miya_wang/Desktop/CMPS_replication/results/NSGA-II' #you should change the path before you run
 dir_path = '/Users/miya_wang/Desktop/Papers/Second_paper/DNN-MT/matlab/'
+out_path = '/Users/miya_wang/Desktop/CMPS_replication/results/NSGA-II/results/'
 
 for dataset in ['fashion','cifar10','imagenet']:
     if dataset == 'fashion':
-        for model in ['lenet1','lenet5']: 
+        for model in ['lenet1','lenet5']:
             print(dataset,model)
             for budget in [500,1000]:
                 print(budget)
                 trc_list = []
                 fdr_list = []
+                all_round_selected = []
                 for i in range(10):
                     errors_list = []
                     all_uniq_pairs = set()
+                    selected_indices = []
                     for mr in mrs.keys():
-                        errors, uniq_pairs = nsga(res_path, dir_path, dataset, model, int(budget/5), mr)
+                        errors, uniq_pairs,selected_elements = nsga(res_path, dir_path, dataset, model, int(budget/5), mr)
+                        selected_indices.append(selected_elements)
                         errors_list.append(errors)
                         all_uniq_pairs |= uniq_pairs
+                    all_round_selected.append(selected_indices)
                     trc = sum(errors_list)/budget
                     fdr = len(all_uniq_pairs)/90
                     trc_list.append(trc)
                     fdr_list.append(fdr)
+                filename = out_path + dataset + '_' + model + '_' + str(budget) + '.pkl'
+                with open(filename, 'w') as f:
+                    json.dump(all_round_selected, f)
                 print("TRC:",sum(trc_list)/10)
                 print("FDR:", sum(fdr_list)/10)
 
@@ -79,17 +87,24 @@ for dataset in ['fashion','cifar10','imagenet']:
                 print(budget)
                 trc_list = []
                 fdr_list = []
+                all_round_selected = []
                 for i in range(10):
                     errors_list = []
                     all_uniq_pairs = set()
+                    selected_indices = []
                     for mr in mrs.keys():
-                        errors, uniq_pairs = nsga(res_path, dir_path, dataset, model, int(budget / 5), mr)
+                        errors, uniq_pairs,selected_elements = nsga(res_path, dir_path, dataset, model, int(budget / 5), mr)
+                        selected_indices.append(selected_elements)
                         errors_list.append(errors)
                         all_uniq_pairs |= uniq_pairs
+                    all_round_selected.append(selected_indices)
                     trc = sum(errors_list) / budget
                     fdr = len(all_uniq_pairs) / 90
                     trc_list.append(trc)
                     fdr_list.append(fdr)
+                filename = out_path + dataset + '_' + model + '_' + str(budget) + '.pkl'
+                with open(filename, 'w') as f:
+                    json.dump(all_round_selected, f)
                 print("TRC:", sum(trc_list) / 10)
                 print("FDR:", sum(fdr_list) / 10)
 
@@ -100,17 +115,24 @@ for dataset in ['fashion','cifar10','imagenet']:
                 print(budget)
                 trc_list = []
                 fdr_list = []
+                all_round_selected = []
                 for i in range(10):
                     errors_list = []
                     all_uniq_pairs = set()
+                    selected_indices = []
                     for mr in mrs.keys():
-                        errors, uniq_pairs = nsga(res_path, dir_path, dataset, model, int(budget/5), mr)
+                        errors, uniq_pairs,selected_elements = nsga(res_path, dir_path, dataset, model, int(budget/5), mr)
+                        selected_indices.append(selected_elements)
                         errors_list.append(errors)
                         all_uniq_pairs |= uniq_pairs
+                    all_round_selected.append(selected_indices)
                     trc = sum(errors_list) / budget
                     fdr = len(all_uniq_pairs) / budget
                     trc_list.append(trc)
                     fdr_list.append(fdr)
+                filename = out_path + dataset + '_' + model+'_'+str(budget) + '.pkl'
+                with open(filename, 'w') as f:
+                    json.dump(all_round_selected, f)
                 print("TRC:", sum(trc_list) / 10)
                 print("FDR:", sum(fdr_list) / 10)
 
